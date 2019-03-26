e94659c3523380e5e22191dc6f1d944f38ec006f
Sanne <sannevanmourik@hotmail.com>
2019-03-26 10:28:16 +0100

    Added FeatureRequestItem model

---
+++ FeatureRequestAPI/FeatureRequestAPI/Data/FeatureRequestAPIContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace FeatureRequestAPI.Models
+{
+    public class FeatureRequestAPIContext : DbContext
+    {
+        public FeatureRequestAPIContext (DbContextOptions<FeatureRequestAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<FeatureRequestAPI.Models.FeatureRequestItem> FeatureRequestItem { get; set; }
+    }
+}
---
+++ FeatureRequestAPI/FeatureRequestAPI/Models/FeatureRequestItem.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Server.Kestrel.Internal.System.Collections.Sequences;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FeatureRequestAPI.Models
+{
+    public class FeatureRequestItem
+    {
+        [Key]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Comment { get; set; }
+        public List<String> Comments = new List<String>();
+        public int NumberOfVotes { get; set; }
+        public Boolean IsDone { get; set; }
+        public Boolean AddedToBacklog { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime LastEditDate { get; set; }
+    }
