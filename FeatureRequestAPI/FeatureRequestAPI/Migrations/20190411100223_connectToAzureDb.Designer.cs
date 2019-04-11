e46326289d4bb2dc871f2e624c734d72c2a1adea
Sanne <sannevanmourik@hotmail.com>
2019-04-11 13:02:14 +0200

    Connection to Azure db is successfull

--- FeatureRequestAPI/FeatureRequestAPI/Migrations/20190411100223_connectToAzureDb.Designer.cs
+++ FeatureRequestAPI/FeatureRequestAPI/Migrations/20190411100223_connectToAzureDb.Designer.cs
@@ -4,14 +4,16 @@ using FeatureRequestAPI.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FeatureRequestAPI.Migrations
 {
     [DbContext(typeof(FeatureRequestAPIContext))]
-    partial class FeatureRequestAPIContextModelSnapshot : ModelSnapshot
+    [Migration("20190411100223_connectToAzureDb")]
+    partial class connectToAzureDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ FeatureRequestAPI/FeatureRequestAPI/Migrations/20190411100223_connectToAzureDb.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FeatureRequestAPI.Migrations
+{
+    public partial class connectToAzureDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
